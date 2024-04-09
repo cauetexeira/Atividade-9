e778c3d89fbff92c9276487a5082c942eb877a48
cauetexeira <160753286+cauetexeira@users.noreply.github.com>
2024-04-09 12:24:57 -0300

    Add files via upload

---
+++ Primeira lista de Java/script.js
@@ -0,0 +1,26 @@
+
+//Atividade 1
+function somar() {
+    var Soma = 0;
+    var Continuar = true;
+    while (Continuar) {  
+    var Número = parseFloat(prompt("Me diz um número ae"));
+    Soma = Soma+Número;
+    Continuar = confirm("Quer mais um número meu fi?");
+}
+alert("A soma dos números é igual a: " +Soma);
+}
+
+
+
+
+//Atividade 2
+
+function contagem() {
+    var Início = 0;
+    var Lançar = true;
+}
+
+
+
+
